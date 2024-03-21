bf41c3234daa980373c15144c44d7d74a9d73728
Saman Siddiqui <samansiddiqui320@gmail.com>
2024-03-22 00:32:54 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+async function startfunc() {
+    const systemNumber = Math.floor(Math.random() * 10);
+    const userNumber = await inquirer.prompt([
+      {
+        type: "number",
+        name: "userGuess",
+        message: "Enter Your Number Between 1 - 10:",
+      },
+    ]);
+   
+const {userGuess} = userNumber;
+
+    console.log("Your Guess: ", userGuess , "\nSyestem Guess: ", systemNumber);
+  
+    if (userGuess === userNumber) {
+      console.log("Your Guess is Correct \nYou Won");
+    } else {
+      console.log("Better luck Next Time");
+    }
+  }
+  
+  async function startAgain() {
+    do{
+      await startfunc();
+      var again = await inquirer
+      .prompt({
+        type: "input",
+        name: "Restart",
+        message: "Do you want to continue? \n Press Y or N ",
+      });
+    }
+     while (
+      (await again).Restart == "y" || (await again).Restart == "Y" || (await again).Restart == "yes" || (await again).Restart == "YES"
+    );
+  }
+
+  startAgain();
